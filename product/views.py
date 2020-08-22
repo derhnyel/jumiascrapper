164c1bc41d457ecfb5d7cabbd3eb1b81a3211d7c
derhnyel <ejedenials@gmail.com>
2020-08-22 11:45:49 +0100

    Update views.py

--- product/views.py
+++ product/views.py
@@ -24,7 +24,7 @@ def index(request):
 def get_link(request):
     try:
         url = request.POST.get('url')
-        url = url['url']
+        #url = url['url']
         if url != None:
             p = scrapper(url)
             return RR(data={'Results': p, 'message': 'SuccessFul'})
