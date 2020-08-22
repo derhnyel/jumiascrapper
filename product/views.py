0f4faf31b4b630e3b2a96e5e514f4af99a39f98b
derhnyel <ejedenials@gmail.com>
2020-08-22 10:29:48 +0100

    Update views.py

--- product/views.py
+++ product/views.py
@@ -13,63 +13,38 @@ from uuid import uuid4
 import os
 import threading
 
-#@require_GET
+@require_GET
 def index(request):
-    #rt = get_link(request)
-    return render(request,'product/index.html',get_link(request))
+    return render(request,'product/index.html')
 
-#@api_view(['POST'])
-#@permission_classes([AllowAny])
-
-
-
-
+@api_view(['POST'])
+@permission_classes([AllowAny])
 def get_link(request) :
-    if request.method == 'POST':
-        #url = request.POST.get('url')
-        link = request.POST.get('textfield',None)
-        result = scrapper(link)
-        #scrapper(url)
-        return result
+    try:
+        url = request.POST.get('url')
+        if None not in (url):
+            p = scrapper(url)      
+            return RR(data={'Results': p, 'message': 'SuccessFul'})
+        else:
+            raise Exception('Invalid URL!')
+    except Exception as e:
+        return RR(data={'success': False, 'message': str(e)}, status=HTTP_400_BAD_REQUEST)
         
         
-
 def scrapper(link):
     response = requests.get(link)
-    raise_error(response)
     soup = bs4s(response.text)
     images_link = []
-
     product_name = soup.select('h1')[0].getText()
-
-
     image_soup = soup.select('#imgs.sldr._img._prod.-rad4.-oh.-mbs a.itm')
-
     product_price_soup = soup.select('div.-hr.-pvs.-mtxs span.-b.-ltr.-tal.-fs24')
-
     for i in range(0, len(image_soup)):
         images_link.append(image_soup[i].get('href'))
-
     product_description = soup.select('div.markup.-mhm.-pvl.-oxa.-sc ')[0].getText()
-
-
     product_price = [i.strip() for i in product_price_soup[0].getText().split('¦')]
-
-    #parse_json(list)
-    result = [product_name, product_price, product_description, images_link]
-    [print (i) for i in result]
-    return {'result':result}
+    result = {'product_name':product_name, 'product_price':product_price, 'product_description':product_description, 'images_link':images_link}
+    return result
     
-
-
-
-
-
-def raise_error(response):
-    try:
-        response.raise_for_status()
-    except Exception as exc:
-        return 'There was a problem %s' % (exc)
 
 
 
